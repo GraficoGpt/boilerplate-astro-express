e59119a723b223a2cc75e715efc86da26a68279d
kashipu <graficowm@gmail.com>
2025-02-11 17:11:50 -0500

    Turso connection ok

--- back-express/modules/hello-world/helloWorldController.js
+++ back-express/modules/hello-world/helloWorldController.js
@@ -1,5 +1,19 @@
-const helloWorld = (req, res) => {
-    res.json({ message: 'Hello World! The service is running properly.' });
-  };
+const tursoClient = require('../../config/turso');
 
-  module.exports = { helloWorld };
+const helloWorld = async (req, res) => {
+  try {
+    // Ejecutar una consulta simple para validar la conexión
+    const result = await tursoClient.execute('SELECT 1');
+
+    if (result) {
+      res.json({ message: 'Hello World! Turso connection is working.' });
+    } else {
+      res.status(500).json({ message: 'Turso connection failed.' });
+    }
+  } catch (error) {
+    console.error('Error checking Turso connection:', error);
+    res.status(500).json({ message: 'Error checking Turso connection.', error: error.message });
+  }
+};
+
+module.exports = { helloWorld };
